d9280bd0cb61d6552c49a913744001c0996c5392
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-06-05 17:52:59 +0200

    defined a function to show error message

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@
     let form = document.querySelector('#contact-form');
     let emailInput = document.querySelector('#email');
     let telInput = document.querySelector('#telephon');
+
+    // show error message
+    function showErrorMessage (input, message){
+        let container = input.parentElement;
+
 })
